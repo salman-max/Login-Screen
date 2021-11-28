0135ea6014c14c95ce7dca2150a300ccfb26648c
Sallahuddin <59514945+salman-max@users.noreply.github.com>
2021-11-28 19:13:59 +0530

    Add files via upload

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    project: {
+        ios: {},
+        android: {},
+    },
+    assets: [newFunction()]
+    };
+
+function newFunction() {
+    return ('LatinoElongatedPlain.ttf');
+}
